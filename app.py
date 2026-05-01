2bea23d96094edf9b33c34bf11cab6092c66a98c
TemplinB <228995889+TemplinB@users.noreply.github.com>
2026-05-01 16:31:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ st.markdown(
     }
     .header-banner {
         background-color: #4B9CD3;
-        padding: 5px;
+        padding: 25px;
         border-radius: 0px;
         text-align: center;
         margin-bottom: 0px;
